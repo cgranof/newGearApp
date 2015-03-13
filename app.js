22be095203b845440219738c37d53d17e79c9f08
Your Actual Name <Your Actual Email>
2015-03-13 16:17:57 -0600

    Test2

--- app.js
+++ app.js
@@ -9,57 +9,21 @@ var passportConfig = require('./config/passport');
 
 
 var indexController = require('./controllers/index.js');
-<<<<<<< HEAD
 var tripsController = require('./controllers/trips.js');
 var authenticationController = require('./controllers/authentication');
-=======
-var gearController = require('./controllers/gear.js');
->>>>>>> 5d71718b84b5283af5138de8cebfc58bfcc2ae67
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/gears');
 
-<<<<<<< HEAD
 // var seedData = require('./models/seeds/gearSeeds.js');
 // seedData.find();
 require('./models/gearModel.json');
-=======
-require('./Models/seeds/gearSeeds.js');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public')); 	
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.get('/', indexController.index);
-
-app.get('/api/gear', gearController.getAll);
-
-
-var server = app.listen(6450, function() {
-	console.log('Express server listening on port ' + server.address().port);
-});
-var express = require('express');
-var bodyParser = require('body-parser');
-var indexController = require('./controllers/index.js');
-var gearController = require('./controllers/gear.js');
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/gears');
-
-require('./models/seeds/gearSeeds.js');
->>>>>>> 5d71718b84b5283af5138de8cebfc58bfcc2ae67
-
-var app = express();
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public')); 	
-
-<<<<<<< HEAD
 app.use(cookieParser());
 app.use(flash());
 app.use(session({
@@ -71,15 +35,12 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-=======
->>>>>>> 5d71718b84b5283af5138de8cebfc58bfcc2ae67
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
 
-<<<<<<< HEAD
 app.get('/mapshome', indexController.mapshome);
 app.get('/gear', tripsController.list);
 
@@ -90,13 +51,7 @@ app.get('/auth/logout', authenticationController.logout);
 
 
 app.use(passportConfig.ensureAuthenticated);
-=======
-app.get('/api/gear', gearController.getAll);
-
-app.get('/templates/:templateid', indexController.getTemplate);
->>>>>>> 5d71718b84b5283af5138de8cebfc58bfcc2ae67
 
 var server = app.listen(6450, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
-
--- models/seeds/gearSeeds.js
+++ models/seeds/gearSeeds.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // var Gear = require('../gear.js');
 
 // module.exports = Gear.find({}, function(err, results){
@@ -27,30 +26,3 @@
 // 	// }
 // });
 
-=======
-var Gear = require('../gear.js');
-
-Gear.find({}, function(err, results){
-	if(results.length === 0) {
-		var list1 = new Gear({
-			location: 'Alaska',
-			activity: 'backpacking',
-			season: 'summer',
-			clothing: [],
-			campgear: [],
-			safety: []
-		});
-		list1.save();
-
-		var list2 = new Gear({
-			location: 'test',
-			activity: 'test',
-			season: 'summer',
-			clothing: [],
-			campgear: [],
-			safety: []
-		});
-		list2.save();
-	}
-});
->>>>>>> 5d71718b84b5283af5138de8cebfc58bfcc2ae67
