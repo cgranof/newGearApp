c8672982b62b5e1d501290f2ae4ad3176eaa2700
Student <carla.granof@gmail.com>
2015-03-17 15:26:51 -0600

    port updated

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ app.use(passportConfig.ensureAuthenticated);
 app.get('/profile', indexController.profile);
 app.post('/update', userController.updateGear);
 
-var server = app.listen(6450, function() {
+var port = process.env.PORT || 6450;
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
